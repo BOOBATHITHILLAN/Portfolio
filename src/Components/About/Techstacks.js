926a7b868f35a28b6932030b4cc523c8f3ba46e6
BOOBATHI THILLAN <128199038+BOOBATHITHILLAN@users.noreply.github.com>
2024-04-21 09:12:54 +0530

    Update Techstacks.js

--- src/Components/About/Techstacks.js
+++ src/Components/About/Techstacks.js
@@ -1,6 +1,6 @@
-import React from "react";
-import "./Techstacks.css";
-import { FaReact, FaNodeJs, FaAws } from "react-icons/fa";
+import React from 'react';
+import './Techstacks.css';
+import { FaReact, FaNodeJs } from 'react-icons/fa';
 import {
   SiRedux,
   SiHtml5,
@@ -9,11 +9,14 @@ import {
   SiNetlify,
   SiMysql,
   SiPostman,
-  SiRender
-} from "react-icons/si";
-import { DiCss3, DiMongodb } from "react-icons/di";
-import { VscGithub } from "react-icons/vsc";
-import { BsBootstrap } from "react-icons/bs";
+  SiTailwindcss,
+  SiRender,
+  SiNextdotjs,
+  SiNestjs,
+} from 'react-icons/si';
+import { DiCss3, DiMongodb } from 'react-icons/di';
+import { VscGithub } from 'react-icons/vsc';
+import { BsBootstrap } from 'react-icons/bs';
 // import {
 //   SiRedux,
 //   SiHtml5,
@@ -32,9 +35,9 @@ import { BsBootstrap } from "react-icons/bs";
 export const Techstacks = () => {
   return (
     <>
-      <div className="section main" data-aos="fade-right">
-        <h2 className="section__title different">Skills</h2>
-        <div className="techsection">
+      <div className='section main' data-aos='fade-right'>
+        <h2 className='section__title different'>Skills</h2>
+        <div className='techsection'>
           <div>
             <SiJavascript />
             <h5>Javascript</h5>
@@ -48,17 +51,26 @@ export const Techstacks = () => {
             <h5>CSS</h5>
           </div>
           <div>
+            <SiNextdotjs />
+            <h5>Next js</h5>
+          </div>
+          <div>
             <FaReact />
-            <h5>React</h5>
+            <h5>React js</h5>
           </div>
           <div>
             <SiRedux />
             <h5>Redux</h5>
           </div>
           <div>
+            <SiNestjs />
+            <h5>Nest js</h5>
+          </div>
+          <div>
             <FaNodeJs />
             <h5>Nodejs</h5>
           </div>
+
           <div>
             <SiExpress />
             <h5>Expressjs</h5>
@@ -76,9 +88,10 @@ export const Techstacks = () => {
             <h5>Bootstrap</h5>
           </div>
           <div>
-            <FaAws />
-            <h5>AWS</h5>
+            <SiTailwindcss />
+            <h5>Tailwind css</h5>
           </div>
+
           <div>
             <SiNetlify />
             <h5>Netlify</h5>
@@ -99,7 +112,6 @@ export const Techstacks = () => {
             <SiRender />
             <h5>Render</h5>
           </div>
-
         </div>
       </div>
     </>
